89419cfbd645b71dbe61affd5ced52f4d5dd30fa
ahemtan <ahemtan1045@gmail.com>
2024-01-26 14:18:32 +0545

    idk

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,14 +3,29 @@ import "./home.scss";
 import FeatureProduct from "../../components/ui/feature";
 
 const Home = () => {
-  return (
-    <>
-      <div className="row">
-        <h1>Hero</h1>
+return (
+<>
+  <div className="hero">
+    <div style={{ backgroundImage: `url(/images/hero.png)`}} className="item">
+      <div className="content">
+        <h1>This is <br /> a the hero</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora
+          quasi magnam minima quod corrupti unde quam incidunt, sed, sunt
+        </p>
+        <button className="btn">Explore Now!</button>
       </div>
-      <FeatureProduct type={"Feature"} />
-    </>
-  );
+
+    </div>
+  </div>
+
+  <FeatureProduct type={"Feature"} />
+
+  <div className="shop">
+    
+  </div>
+</>
+);
 };
 
 export default Home;
